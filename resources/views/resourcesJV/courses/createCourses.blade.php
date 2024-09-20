256a3f6208b84cd5d4f2b4cf54a01cfb75997781
madai120 <madaictun@gmail.com>
2024-09-20 03:21:46 +0000

    componente alert-message

--- resources/views/resourcesJV/courses/createCourses.blade.php
+++ resources/views/resourcesJV/courses/createCourses.blade.php
@@ -12,23 +12,7 @@
             class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Agregar</button>
     </form>
 
-    @if (session('message'))
-        <script>
-            swal("Éxito", "{{ session('message') }}", 'success', {
-                button: true,
-                button: "OK",
-            });
-        </script>
-    @endif
-
-    @if (session('error'))
-        <script>
-            swal("Error", "{{ session('error') }}", 'error', {
-                button: true,
-                button: "OK",
-            });
-        </script>
-    @endif
+    <x-alert-message />
 
 
 </div>
