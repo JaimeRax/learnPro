9fb205c72ff3b4b031033a99fa1c868992d0e248
madai120 <madaictun@gmail.com>
2024-10-07 09:01:29 +0000

    Design PDF update

--- resources/views/pdf/myPDF.blade.php
+++ resources/views/pdf/myPDF.blade.php
@@ -26,7 +26,6 @@
                 background: #151819;
                 color: #FFFFFF;
                 text-align: center;
-                vertical-align: top;
                 height: 22px;
                 border-radius: 3px;
             }
@@ -85,13 +84,15 @@
             }
 
             .content {
-                border-bottom: 0.7px solid #2c2e35;
-                display: inline-block;
-                font-weight: normal;
-                line-height: 1;
-                text-align: center;
-                padding-top: 8px;
-            }
+    border-bottom: 0.7px solid #2c2e35;
+    display: inline-block;
+    font-size: 0.8rem;
+    line-height: 1;
+    text-align: center;
+    padding-top: 8px;
+    font-weight: normal; /* Esto asegura que el texto no esté en negrita */
+}
+
 
             .header {
                 line-height: 0.3;
@@ -155,6 +156,10 @@
                 padding: 0;
                 /* Elimina padding si es necesario */
             }
+
+            .hr-firma {
+            margin-top: 4rem;
+        }
         </style>
     </head>
 
@@ -176,24 +181,26 @@
                 </div>
 
             </div>
+            <div style=" font-size: 0.8rem;">
+                <p class=" lato-bold">No. 000001</p>
+            </div>
 
-
-            <h4 class="titulo" style="margin-bottom:5px; margin-top: 40px;">
+            <h4 class="titulo" style=" font-size: 0.7rem;">
                 Datos Generales del Pago
             </h4>
 
-            <table class="table mt-5 table-offset" style=" margin-top: 15px;">
+            <table class="table mt-5 table-offset" style=" margin-top: 10px;">
                 <thead>
                     <tr>
-                        <th scope="col" style=" font-size: 0.7rem;">Cuota Escolar Mensual</th>
-                        <th scope="col" style=" font-size: 0.7rem;">Couta Computaciòn Mneusal</th>
+                        <th scope="col" style=" font-size: 0.5rem;">Cuota Escolar Mensual</th>
+                        <th scope="col" style=" font-size: 0.5rem;">Couta Computaciòn Mneusal</th>
 
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td style=" font-size: 0.7rem;">Q55.00</td>
-                        <td style=" font-size: 0.7rem;">q20.00</td>
+                        <td style=" font-size: 0.6rem;">Q55.00</td>
+                        <td style=" font-size: 0.6rem;">q20.00</td>
 
 
                     </tr>
@@ -201,47 +208,62 @@
             </table>
 
             <div style="flex-direction: column;">
-                <div style="flex-direction:row; margin-top: 25px;">
-                    <span class="campo" style=" font-size: 0.7rem;">Nombre:</span>
-                    <span class="content" style="width:92%">
+                <div style="flex-direction:row; margin-top: 15px;">
+                    <span class="campo" style=" font-size: 0.8rem;">Nombre:</span>
+                    <span class="content" style="width:35.5%">
                         {{ $dato->first_lastname }} {{ $dato->second_lastname }} {{ $dato->first_name }}
                         {{ $dato->second_name }}
                     </span>
+                    <span class="campo" style=" font-size: 0.8rem; margin-left: 35.5px;">Fecha:</span>
+                    <span class="content" style="width:41.5%">{{ \Carbon\Carbon::now()->format('d/m/Y') }}</span>
 
+                </div>
+
+                <div style="flex-direction:row; margin-top: 1px;">
+                    <span class="campo" style=" font-size: 0.8rem;">Grado:</span>
+                    <span class="content" style="width:37% ">Primero</span>
+                    <span class="campo" style="font-size: 0.8rem; margin-left: 35.3px;">Secciòn: </span>
+                    <span class="content" style="width:40%"> A </span>
 
                 </div>
 
-                <div style="flex-direction:row; margin-top: 5px;">
-                    <span class="campo" style=" font-size: 0.7rem;">Fecha:</span>
-                    <span class="content" style="width:93.5%">{{ \Carbon\Carbon::now()->format('d/m/Y') }}</span>
+                <div style="flex-direction:row; margin-top: 1px;">
 
+                    <span class="campo" style="font-size: 0.8rem;">Mes de pago: </span>
+                    <span class="content" style="width:31%"> Enero </span>
+                    <span class="campo" style=" font-size: 0.8rem; margin-left: 31px;"> Tipo de pago:</span>
+                    <span class="content" style="width:35.8%"> Mensualidad </span>
                 </div>
-                <div style="flex-direction:row; margin-top: 5px;">
 
-                    <span class="campo" style="font-size: 0.7rem;">Metodo de pago: </span>
-                    <span class="content" style="width:85.5%"> Efectivo </span>
-                </div>
-                <div style="flex-direction:row; margin-top: 5px;">
+                <div style="flex-direction:row; margin-top: 10px;">
 
-                    <span class="campo" style=" font-size: 0.7rem;"> Tipo de pago:</span>
-                    <span class="content" style="width:88%"> Mensualidad </span>
+                    <span class="campo" style=" font-size: 0.8rem;"> Observaciones</span>
+                    <span class="content" style="width:84.2%; font-size: 0.8rem;"> jhbjkhbhbkhbjk jsdnkljfa jansdlfk jas jnksdf </span>
                 </div>
 
 
 
-                <div style="flex-direction:row; margin-top: 30px;">
+                <div style="flex-direction:row; margin-top: 10px;">
 
-                    <span class="campo" style=" font-size: 0.7rem;"> Total:</span>
-                    <span class="" style="width:94.5%"> Q. {{ $dato->personal_code }} </span>
+                    <span class="campo" style=" font-size: 0.8rem;"> Total:</span>
+                    <span class="" style="width:94.5%; font-size: 0.7rem;"> Q. {{ $dato->personal_code }} </span>
                 </div>
 
+                <div class="mt-3 mb-3 text-center" >
+                    <div>
+                        <hr class="hr-firma" style="width: 50%;">
+                        <p style=" margin-left: 280px; font-size: 0.7rem;">Firma de Direcciòn</p>
 
+                    </div>
+                </div>
             </div>
         @endforeach
 
+
+
         @foreach ($payments as $dato)
-        <div class="header" style=" margin-top: 150px;">
-            <div class="contact-info lato-regular">
+        <div class="header" style=" margin-top: 80px;">
+            <div class="contact-info lato-regular" >
                 <p>4ta. Ave 0-37 zona 4, Finca Municipal Sesbiché San Juan Chamelco Alta Verapaz</p>
                 <p>institutobasicojv.chamelco@gmail.com</p>
                 <p>Tel. (+502) 5991 0548</p>
@@ -256,23 +278,26 @@
 
         </div>
 
+        <div style=" font-size: 0.8rem;">
+            <p class=" lato-bold">No. 000001</p>
+        </div>
 
-        <h4 class="titulo" style="margin-bottom:5px; margin-top: 50px;">
+        <h4 class="titulo" style=" font-size: 0.7rem;">
             Datos Generales del Pago
         </h4>
 
-        <table class="table mt-5 table-offset" style=" margin-top: 15px;">
+        <table class="table mt-5 table-offset" style=" margin-top: 10px;">
             <thead>
                 <tr>
-                    <th scope="col" style=" font-size: 0.7rem;">Cuota Escolar Mensual</th>
-                    <th scope="col" style=" font-size: 0.7rem;">Couta Computaciòn Mneusal</th>
+                    <th scope="col" style=" font-size: 0.5rem;">Cuota Escolar Mensual</th>
+                    <th scope="col" style=" font-size: 0.5rem;">Couta Computaciòn Mneusal</th>
 
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td style=" font-size: 0.7rem;">Q55.00</td>
-                    <td style=" font-size: 0.7rem;">q20.00</td>
+                    <td style=" font-size: 0.6rem;">Q55.00</td>
+                    <td style=" font-size: 0.6rem;">q20.00</td>
 
 
                 </tr>
@@ -280,41 +305,54 @@
         </table>
 
         <div style="flex-direction: column;">
-            <div style="flex-direction:row; margin-top: 25px;">
-                <span class="campo" style=" font-size: 0.7rem;">Nombre:</span>
-                <span class="content" style="width:92%">
+            <div style="flex-direction:row; margin-top: 15px;">
+                <span class="campo" style=" font-size: 0.8rem;">Nombre:</span>
+                <span class="content" style="width:35.5%">
                     {{ $dato->first_lastname }} {{ $dato->second_lastname }} {{ $dato->first_name }}
                     {{ $dato->second_name }}
                 </span>
+                <span class="campo" style=" font-size: 0.8rem; margin-left: 35.5px;">Fecha:</span>
+                <span class="content" style="width:41.5%">{{ \Carbon\Carbon::now()->format('d/m/Y') }}</span>
 
+            </div>
+
+            <div style="flex-direction:row; margin-top: 1px;">
+                <span class="campo" style=" font-size: 0.8rem;">Grado:</span>
+                <span class="content" style="width:37% ">Primero</span>
+                <span class="campo" style="font-size: 0.8rem; margin-left: 35.3px;">Secciòn: </span>
+                <span class="content" style="width:40%"> A </span>
 
             </div>
 
-            <div style="flex-direction:row; margin-top: 5px;">
-                <span class="campo" style=" font-size: 0.7rem;">Fecha:</span>
-                <span class="content" style="width:93.5%">{{ \Carbon\Carbon::now()->format('d/m/Y') }}</span>
+            <div style="flex-direction:row; margin-top: 1px;">
 
+                <span class="campo" style="font-size: 0.8rem;">Mes de pago: </span>
+                <span class="content" style="width:31%"> Enero </span>
+                <span class="campo" style=" font-size: 0.8rem; margin-left: 31px;"> Tipo de pago:</span>
+                <span class="content" style="width:35.8%"> Mensualidad </span>
             </div>
-            <div style="flex-direction:row; margin-top: 5px;">
 
-                <span class="campo" style="font-size: 0.7rem;">Metodo de pago: </span>
-                <span class="content" style="width:85.5%"> Efectivo </span>
-            </div>
-            <div style="flex-direction:row; margin-top: 5px;">
+            <div style="flex-direction:row; margin-top: 10px;">
 
-                <span class="campo" style=" font-size: 0.7rem;"> Tipo de pago:</span>
-                <span class="content" style="width:88%"> Mensualidad </span>
+                <span class="campo" style=" font-size: 0.8rem;"> Observaciones</span>
+                <span class="content" style="width:84.2%; font-size: 0.8rem;"> jhbjkhbhbkhbjk jsdnkljfa jansdlfk jas jnksdf </span>
             </div>
 
 
 
-            <div style="flex-direction:row; margin-top: 30px;">
+            <div style="flex-direction:row; margin-top: 10px;">
 
-                <span class="campo" style=" font-size: 0.7rem;"> Total:</span>
-                <span class="" style="width:94.5%"> Q. {{ $dato->personal_code }} </span>
+                <span class="campo" style=" font-size: 0.8rem;"> Total:</span>
+                <span class="" style="width:94.5%; font-size: 0.7rem;"> Q. {{ $dato->personal_code }} </span>
             </div>
 
+            <div class="mt-3 mb-3 text-center">
+                <div>
+                    <hr class="hr-firma" style="width: 50%;">
+                    <p style=" margin-left: 280px; font-size: 0.7rem;">Firma de Direcciòn</p>
 
+                </div>
+            </div>
         </div>
     @endforeach
     </body>
