4aa8b15f7582c083965037b665f5232b240c7bc9
madai120 <madaictun@gmail.com>
2024-10-09 04:55:02 +0000

    delete button PDF in vies newPayment

--- resources/views/payments/newPayment.blade.php
+++ resources/views/payments/newPayment.blade.php
@@ -132,8 +132,6 @@
         <button type="submit"
             class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Agregar</button>
 
-        <a id="createPayment-{{ $studens->id }}" href="{{ url('payments/pdf_generator', $studens->id) }}"
-            class="btn btn-secondary">PDF</a>
 
         <hr class="mt-4 border-gray-300 border-solid rounded-full mb-7 border-1">
 
