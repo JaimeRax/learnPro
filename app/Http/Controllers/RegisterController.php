24d4a017b80287429680e7d5a5791ec47c301be3
madai120 <madaictun@gmail.com>
2024-10-10 07:41:58 +0000

    include role registration in create user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,6 +9,7 @@ use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Auth;
 use lluminate\Http\RedirectResponse;
 use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Log;
 
 class RegisterController extends Controller
 {
@@ -22,11 +23,28 @@ class RegisterController extends Controller
     }
 
 
-    public function register(RegisterRequest $request){
-        $user = User::create($request->validated());
-        $roles = $request->input('roles', []);
-        $user->syncRoles($roles);
-        return redirect('/login', $user->id)->with('success', 'Usuario creado correctamente');
+    public function register(RegisterRequest $request)
+    {
+        try {
+            $user = User::create($request->validated());
+
+            $roles = $request->input('roles', []);
+            if (!is_array($roles)) {
+                $roles = [$roles];
+            }
+
+            $roleNames = Role::whereIn('id', $roles)->pluck('name')->toArray();
+            Log::info('Roles a sincronizar:', ['roles' => $roleNames]);
+
+            $user->syncRoles($roleNames);
+            Log::info('Roles sincronizados para el usuario:', ['user_id' => $user->id]);
+
+            return redirect('/login')->with('success', 'Usuario creado correctamente');
+        } catch (\Exception $e) {
+            Log::error('Error al registrar el usuario: ' . $e->getMessage());
+
+            return redirect()->back()->withErrors(['error' => 'Hubo un problema al crear el usuario. Inténtalo nuevamente.']);
+        }
     }
 
 
