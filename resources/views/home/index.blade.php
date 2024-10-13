2bb666e1c65cc34ff4a3b91186366d062d3a1b56
root <etetcut@miumg.edu.gt>
2024-10-13 09:58:40 +0000

    diseño del home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,13 +2,37 @@
 
 @section('main')
     @auth
-        <div class="flex flex-col items-center justify-center h-screen bg-white">
-            <div class="max-w-2xl p-8 rounded-lg shadow-lg bg-gray-50">
-                <h1 class="mb-4 text-3xl font-extrabold text-gray-900">Bienvenido a la Página</h1>
-                <p class="text-lg text-gray-700">
-                    Hola, {{ auth()->user()->name ?? auth()->user()->username }}. Estás autenticado.
-                </p>
+    <div class="flex flex-col items-center justify-center h-screen bg-white">
+        <!-- Imagen en la parte superior -->
+        <div class="flex flex-col items-center mb-20">
+            <img class="w-40 h-auto" src="/Imagenes/jv-logo.png" alt="jv-logo" />
+        </div>
+        <!-- Contenedor de columnas -->
+        <div class="flex flex-row items-stretch justify-center space-x-4">
+            <div class="max-w-xs p-8 text-sm text-justify rounded-lg shadow-lg w-80 bg-gray-50 h-96">
+                <h1 class="mb-4 text-2xl font-extrabold text-gray-900">Misión</h1>
+                <h2>Somos una Institución Educativa con modalidad Cooperativa, con el propósito de brindar una educación
+                    integral a los estudiantes, en un ambiente agradable, armónico y seguro, donde se respeta, valora y se
+                    fortalece las características, habilidades y aptitudes individuales de los estudiantes; formando ciudadanos
+                    líderes, creativos, reflexivos, comunicativos, para ser personas de éxito.</h2>
+            </div>
+            <div class="max-w-xs p-8 text-sm text-justify rounded-lg shadow-lg w-80 bg-gray-50 h-96">
+                <h1 class="mb-4 text-2xl font-extrabold text-gray-900">Visión</h1>
+                <h2>Ser una Institución Educativa líder de la región, en la formación académica de excelencia, con principios y
+                    valores para alcanzar una educación sólida que ayude al estudiante a desarrollarse como ciudadano útil, en
+                    los distintos aspectos de la vida.</h2>
+            </div>
+            <div class="max-w-xs p-8 text-sm text-justify rounded-lg shadow-lg w-80 bg-gray-50 h-96">
+                <h1 class="mb-4 text-2xl font-extrabold text-gray-900">Valores</h1>
+                <div class="flex flex-col space-y-2">
+                    <span class="block">DISCIPLINA</span>
+                    <span class="block">RESPETO</span>
+                    <span class="block">ORDEN</span>
+                    <span class="block">LIMPIEZA</span>
+                </div>
             </div>
         </div>
+    </div>
+
     @endauth
 @endsection
