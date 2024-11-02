f921ab4bfb4186f62f60299003d8c152affba8e9
madai120 <madaictun@gmail.com>
2024-11-02 00:45:34 +0000

    design-name

--- resources/views/resourcesJV/courses/listCourses.blade.php
+++ resources/views/resourcesJV/courses/listCourses.blade.php
@@ -72,7 +72,7 @@
                 @foreach ($courses as $course)
                     <x-tablas.tr>
                         <x-tablas.td>{{ $i++ }}</x-tablas.td>
-                        <x-tablas.td>{{ $course->name }}</x-tablas.td>
+                        <x-tablas.td>{{ strtoupper("{$course->name}") }}</x-tablas.td>
                         <x-tablas.td>
 
                             {{-- modal para desactivar un curso --}}
