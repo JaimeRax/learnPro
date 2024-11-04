bc09adcae46f52e16c20343a19789fbd2a9a1ca8
madai120 <madaictun@gmail.com>
2024-11-04 06:47:01 +0000

    admin file structure

--- resources/views/layouts/navigator/roles/admin.blade.php
+++ resources/views/layouts/navigator/roles/admin.blade.php
@@ -3,87 +3,66 @@
 
 
     @can('admin')
+        <li>
 
-    <li>
+            <details>
 
-        <details>
+                <summary><i class="fa-solid fa-briefcase"></i>Recursos</summary>
 
-        <summary><i class="fa-solid fa-briefcase"></i>
-            Recursos</summary>
+                <ul>
 
-        <ul>
+                    <li><a href="{{ '/sections' }}"><i class="fa-solid fa-folder"></i>Secciones</a></li>
 
-        <li><a href="{{ '/sections' }}"><i class="fa-solid fa-folder"></i>
-            Secciones</a></li>
+                    <li><a href="{{ '/degrees' }}"><i class="fa-solid fa-graduation-cap"></i>Grados</a></li>
 
-        <li><a href="{{ '/degrees' }}"><i class="fa-solid fa-graduation-cap"></i>
-            Grados</a></li>
+                    <li><a href="{{ '/courses' }}"><i class="fa-solid fa-chalkboard-teacher"></i> Cursos</a></li>
 
-        <li><a href="{{ '/courses' }}"><i class="fa-solid fa-chalkboard-teacher"></i>
-            Cursos</a></li>
+                    <li><a href="{{ '/collaborations' }}"><i class="fa-solid fa-coins"></i> Colaboraciones</a></li>
+                </ul>
 
-        <li><a href="{{ '/collaborations' }}"><i class="fa-solid fa-coins"></i>
-            Colaboraciones</a></li>
-
+            </details>
 
         </li>
 
-        </ul>
+        </li>
 
-        </details>
+        <li><a href="{{ '/student' }}"><i class="fa-solid fa-user-plus"></i> Estudiantes</a></li>
 
-    </li>
+        <li><a href="{{ '/payments' }}"><i class="fa-solid fa-money-bill-wave"></i> Pagos</a></li>
 
-    @endcan
+        <li>
 
+            <details>
 
-    @can('admin')
+                <summary><i class="fa-solid fa-tasks"></i>Asignaciones generales</summary>
 
-    <li><a href="{{ '/student' }}"><i class="fa-solid fa-user-plus"></i> Estudiantes</a></li>
-
-    <li><a href="{{ '/payments' }}"><i class="fa-solid fa-money-bill-wave"></i>
-        Pagos</a></li>
-
-    <li>
-
-        <details>
-
-        <summary><i class="fa-solid fa-tasks"></i>
-            Asignaciones generales</summary>
-
-        <ul>
-            <li><a href="{{ '/teachers' }}"><i class="fa-solid fa-user-check"></i>
-                Asignaciones de Docentes</a></li>
+                <ul>
+                    <li><a href="{{ '/teachers' }}"><i class="fa-solid fa-user-check"></i>Asignaciones de Docentes</a>
+                    </li>
 
 
-            <li><a href="{{ '/assignment/student' }}"><i class="fa-solid fa-clipboard-list"></i>
-                Asignaciones de estudiantes</a></li>
-        </ul>
+                    <li><a href="{{ '/assignment/student' }}"><i class="fa-solid fa-clipboard-list"></i> Asignaciones de
+                            estudiantes</a></li>
+                </ul>
 
-        </details>
+            </details>
 
-    </li>
+        </li>
 
-    <li><a href="{{ '/users' }}"><i class="fa-solid fa-users"></i>
-        Usuarios</a></li>
+        <li><a href="{{ '/users' }}"><i class="fa-solid fa-users"></i>Usuarios</a></li>
 
-    <li><a href="{{ '/report/payments' }}"><i class="fa-solid fa-file-alt"></i>
-        Reportes</a></li>
-
-
+        <li><a href="{{ '/report/payments' }}"><i class="fa-solid fa-file-alt"></i>Reportes</a></li>
     @endcan
 
 
     @can('teacher')
+        <li><a href="{{ '/teachers/myCourses' }}"><i class="fa-solid fa-book"></i> Mis cursos</a></li>
 
-    <li><a href="{{ '/teachers/myCourses' }}"><i class="fa-solid fa-book"></i> Mis cursos</a></li>
+        <li><a href="{{ '/ratings' }}"><i class="fa-solid fa-clipboard-check"></i> Mis notas</a></li>
 
-    <li><a href="{{ '/ratings' }}"><i class="fa-solid fa-clipboard-check"></i> Mis notas</a></li>
+        <li><a href="{{ '/activity' }}"><i class="fa-solid fa-calendar-alt"></i> Mis actividades</a></li>
 
-    <li><a href="{{ '/activity' }}"><i class="fa-solid fa-calendar-alt"></i>  Mis actividades</a></li>
-    <li><a href="{{ '/report/report' }}"><i class="fa-solid fa-file-alt"></i> Reportes</a></li>
-
-
+        <li><a href="{{ '/report/report' }}"><i class="fa-solid fa-file-alt"></i> Reportes</a></li>
     @endcan
 
 
